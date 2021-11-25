c5db78092347224405ac4130fe4b7ab0af6cf7bf
TooCoolForStool <dragonofdagon@gmail.com>
2021-11-25 17:18:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def main():
 
 if __name__ == '__main__':
     
-    app.run(host="0.0.0.0", port=8080, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
